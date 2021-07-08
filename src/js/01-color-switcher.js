b70204da6e8397120f65cfdf5ae500fe161d7934
OksanaMosendz <bagheera03@gmail.com>
2021-07-09 01:53:30 +0300

    hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body=document.querySelector('body');
+let intervalId=0;
+
+function getRandomHexColor(){
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startSwitch=()=>{
+intervalId=setInterval(()=>{body.style.backgroundColor=`${getRandomHexColor()}`;}, 1000);
+};
+
+const stopSwitch=()=>{
+  clearInterval(intervalId);
+};
+
+startBtn.addEventListener('click',startSwitch);
+stopBtn.addEventListener('click',stopSwitch);
 
